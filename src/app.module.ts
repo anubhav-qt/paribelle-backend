6219bc8a65ec3bcbef0023270868c285924d47dc
Anil Joshi <ajaniljoshijobs@gmail.com>
2025-12-07 10:44:48 +0100

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,10 +51,13 @@ import { AppResolver } from './app.resolver';
         
         // Connection pooling for better performance
         extra: {
+          ssl: {
+            rejectUnauthorized: false, // Required for Render PostgreSQL
+          },
           max: 20, // Maximum number of connections in the pool
           min: 5,  // Minimum number of connections in the pool
           idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
-          connectionTimeoutMillis: 2000, // Timeout when connecting
+          connectionTimeoutMillis: 10000, // Timeout when connecting (10 seconds for Render)
         },
       }),
       inject: [ConfigService],
